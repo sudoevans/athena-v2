2bc92e9d4c7ad43516dcc2a91ac2259340f452e9
Gamer Snave <snaveqiptoo@gmail.com>
2023-11-28 14:28:31 +0300

    Prevent replies to group messages

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const wa = require('@open-wa/wa-automate');
+const { default: PQueue } = require("p-queue");
+
 const generateResponse = require('./bard');
+
+
+const queue = new PQueue({ concurrency: 1,
+autoStart:false});
 
 wa.create({
   sessionId: "COVID_HELPER",
@@ -14,34 +20,36 @@ wa.create({
   qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
 }).then(client => start(client));
 
+
+// Disable Group Chats.
 function start(client) {
   client.onMessage(async message => {
-    if (message.body != '') 
-    {
-      // Use the message content to generate a response
-      const response = await generateResponse(message.body);
-      // await client.reply(message.from, response);
-      await client.sendText(message.from, response);
+
+   
+    // if (message.body != '') 
+    // {
+    //   // Use the message content to generate a response
+      // const response = await generateResponse(message.body);
+    //   // await client.reply(message.from, response);
+    //   await client.sendText(message.from, response);
+    // }
+    try {
+      // Check if the message is from a group
+      const isGroupMessage = message.isGroupMsg;
+
+      // If it's not a group message, reply
+      if (!isGroupMessage) {
+        const response = await generateResponse(message.body); //Making sure the message is not from group before sending a response.
+        await client.startTyping(message.from); //Simulating typing action.
+        await client.sendText(message.from, response);
+      }
+    } catch (error) {
+      console.error('Error processing message:', error);
     }
   });
 
-
-
 }
 
-
-
-
-
-
-// // Example usage
-// generateResponse('Hello, where is Karatina?')
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
 
 
   // function start(client) {
